b98a168461a2410a36f8ac08aa0573d3f84b721c
sanjai2002 <sanjaihari2002@gmail.com>
2024-03-14 09:43:12 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//  <>
+//  <h1 data-testid="MyName">Sanjai-Trainee Software Engineer</h1>
+//  <h3 data-testid="Mycompany">Working in Relevantz</h3>
+//  </>
+//   );
+// }
+// export default App;
+import React from 'react';
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={Num1:"",Num2:"",Result:""};
+  }
+  Add=()=>{
+    var num1=parseInt(this.state.Num1);
+    var num2=parseInt(this.state.Num2);
+    var sum=num1+num2;
+    this.setState({Result:sum});
+  }
+  render(){
+    return(
+<>
+<h1 data-testid="Heading">Adding two number</h1>
+<label data-testid="Label1">Enter the number 1:</label>
+<input type="number"value={this.state.Num1} data-testid="input1"onChange={(e)=>{this.setState({Num1:e.target.value})}}/>
+<br></br>
+<label data-testid="Label2">Enter the number 2:</label>
+<input type="number"value={this.state.Num2} data-testid="input2" onChange={(e)=>{this.setState({Num2:e.target.value})}}/>
+<br></br>
+<input type='button' value="Add" data-testid="Button"onClick={this.Add}/>
+<br></br>
+<b data-testid="Result">sum:<span Style={"color:red"}>{this.state.Result}</span></b>
+
+</>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,92 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('My test',()=>{
+  // test('Testcase-1',()=>{
+  //   render(<App/>);
+  //   var test=screen.getByTestId("MyName");
+  //   expect(test).toBeInTheDocument();
+  //   expect(test).toHaveTextContent('Sanjai-Trainee Software Engineer');
+
+  //   });
+
+  // // it or test  
+  //   it('Testcase-2',()=>{
+  //     render(<App/>);
+  //     const test=screen.getByText('Working in Relevantz');
+  //     expect(test).toBeInTheDocument();
+    
+  //   });
+
+  test('Test case-1[Render all ui]',()=>{
+    render(<App/>);
+    const input1=screen.getByTestId( 'input1');
+    const input2=screen.getByTestId( 'input2');
+    const Button=screen.getByTestId( 'Button');
+
+    expect(input1).toBeInTheDocument();
+    expect(input2).toBeInTheDocument();
+    expect(Button).toBeInTheDocument();
+
+  })
+
+  test('Test case-2[Render All labels and text message]',()=>{
+    render(<App />);
+
+    const Heading=screen.getByTestId( "Heading");
+    const Label1=screen.getByTestId( "Label1");
+    const Label2=screen.getByTestId( "Label2");
+    const Result=screen.getByTestId( "Result");
+
+    expect(Heading).toBeInTheDocument();
+    expect(Heading).toHaveTextContent('Adding two number')
+
+    expect(Label1).toBeInTheDocument();
+    expect(Label1).toHaveTextContent('Enter the number 1:')
+
+    expect(Label2).toBeInTheDocument();
+    expect(Label2).toHaveTextContent('Enter the number 2:')
+
+    expect(Result).toBeInTheDocument();
+    expect(Result).toHaveTextContent('sum:')
+
+  });
+
+  test('Test case-3[Testing Textboxes Initial value blank]',()=>{
+  render(<App/>)
+  const input1=screen.getByTestId( 'input1');
+  const input2=screen.getByTestId( 'input2');
+  expect(input1).toHaveTextContent('');
+  expect(input2).toHaveTextContent('');
+  });
+  
+  test('test case-4[Testing for no input error message]',()=>{
+    render(<App/>);
+    const Button=screen.getByTestId('Button');
+    fireEvent.click(Button);
+    const Result=screen.getByTestId("Result");
+    expect(Result).toHaveTextContent('sum:NaN');
+  
+  })
+  
+test('test case-5 [Testing for success result]',()=>{
+  render(<App/>);
+  const input1=screen.getByTestId( 'input1');
+  const input2=screen.getByTestId('input2');
+  const button =screen.getByTestId('Button');
+
+  fireEvent.change(input1,{target: {value:10}});
+  fireEvent.change(input2,{target: {value:30}});
+  fireEvent.click(button);
+
+  const result= screen.getByTestId('Result');
+  expect(result).toHaveTextContent("sum:40");
+
+})
+
+
 });
+
+
+
+
